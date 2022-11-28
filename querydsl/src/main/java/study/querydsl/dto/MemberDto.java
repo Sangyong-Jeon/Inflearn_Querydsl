0e6b698246264d251ab95537da8984252029c64e
JeonSangYong <tkddyd420@naver.com>
2022-11-28 11:24:04 +0900

    [강의] 프로젝션과 결과 반환 - DTO 조회

---
+++ querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -0,0 +1,23 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import study.querydsl.entity.Member;
+
+@Data
+@NoArgsConstructor
+public class MemberDto {
+
+    private String username;
+    private int age;
+
+    public MemberDto(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public MemberDto(Member member) {
+        this.username = member.getUsername();
+        this.age = member.getAge();
+    }
+}
