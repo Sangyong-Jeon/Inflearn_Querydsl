215dbcc2fba9c4cdfec1c12e7a9fb9aef854b0ea
JeonSangYong <tkddyd420@naver.com>
2022-11-29 14:46:51 +0900

    [강의] 프로젝션과 결과 반환 - @QueryProjection

--- querydsl/src/main/java/study/querydsl/dto/MemberDto.java
+++ querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import study.querydsl.entity.Member;
@@ -11,6 +12,7 @@ public class MemberDto {
     private String username;
     private int age;
 
+    @QueryProjection // 이거 붙이고 compileQuerydsl하면 해당 DTO도 Q파일로 생성됨
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
